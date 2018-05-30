cab6f96daf17a7433be84e4bde032dbc0b0c5dce
laizihan <369070525qq@gmail.com>
2018-05-30 15:43:30 +0800

    继续删除

--- app/src/main/java/com/example/laizihan/simpleprojects/SimpleJava.java
+++ app/src/main/java/com/example/laizihan/simpleprojects/SimpleJava.java
@@ -5,16 +5,22 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
+
+import java.util.Hashtable;
 
 public class SimpleJava {
 
     public static Bitmap createCode(Context context, String content) throws WriterException {
         //生成二维矩阵,编码时指定大小,不要生成了图片以后再进行缩放,这样会模糊导致识别失败
+        Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+        hints.put(EncodeHintType.MARGIN, 1);
         BitMatrix matrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, 250,
-                250);
+                250, hints);
         int width = matrix.getWidth();
         int height = matrix.getHeight();
         //二维矩阵转为一维像素数组,也就是一直横着排了
