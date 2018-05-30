47e606f11e580c00aea7eb1c28f21a95eb1d5792
laizihan <369070525qq@gmail.com>
2018-05-30 15:22:20 +0800

    删除无用资源

--- app/src/main/java/com/example/laizihan/simpleprojects/MainActivity.kt
+++ app/src/main/java/com/example/laizihan/simpleprojects/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.laizihan.simpleprojects
 
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.common.BitMatrix
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView.setImageBitmap(SimpleJava.createCode(this,"https://www.baidu.com"))
     }
+
+
 }
